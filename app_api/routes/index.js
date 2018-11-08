68af9bbd6bd759bf67bcd6a51bcb41d4b902af29
Lakhbir Bansal <Lbansal1804@conestogac.on.ca>
2018-11-07 19:11:29 -0500

    Testing without View Part

---
+++ app_api/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlFood = require('../controllers/food');
+
+// food
+router.get('/food', ctrlFood.foodDetail);
+router.post('/food', ctrlFood.foodCreate);
+router.get('/food/:foodid', ctrlFood.foodReadOne);
+router.put('/food/:foodid', ctrlFood.foodUpdateOne);
+router.delete('/food', ctrlFood.foodDeleteOne);
+
+module.exports = router;
